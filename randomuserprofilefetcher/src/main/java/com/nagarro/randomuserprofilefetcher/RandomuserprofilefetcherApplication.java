5ff47e895d59718481eac129816e9a7026ae25a7
ayush6183248 <71009252+ayush6183248@users.noreply.github.com>
2024-01-21 17:24:30 +0530

    Update RandomuserprofilefetcherApplication.java

--- randomuserprofilefetcher/src/main/java/com/nagarro/randomuserprofilefetcher/RandomuserprofilefetcherApplication.java
+++ randomuserprofilefetcher/src/main/java/com/nagarro/randomuserprofilefetcher/RandomuserprofilefetcherApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RandomuserprofilefetcherApplication {
-
+//EntryPoint of application
 	public static void main(String[] args) {
 		SpringApplication.run(RandomuserprofilefetcherApplication.class, args);
 	}
